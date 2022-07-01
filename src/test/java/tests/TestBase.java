c33585fa934f7c4cf2ed2fbd3752f06b37265cb0
Mikhail Malygin <mihail.malygin@gmail.com>
2022-07-01 09:49:55 +0500

    added property browserSize

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,6 +28,7 @@ public class TestBase {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true); // включение потовокого вещание видео
         capabilities.setCapability("enableVideo", true); // запись видео теста, записывается на сервер селеноида
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = format("https://%s:%s@%s", login, password, System.getProperty("selenoidStand"));
 
